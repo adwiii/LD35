3845c157f7115f8cf19789ea9317c21dc23a2285
adwiii <adwiii7@gmail.com>
2016-04-18 20:05:31 -0400

    Adjusted TutorialLevel timings and behavior for state transitions

--- LD35/src/TutorialLevel.java
+++ LD35/src/TutorialLevel.java
@@ -24,14 +24,16 @@ public class TutorialLevel extends Level implements KeyListener {
 			//3
 			"Press Down Arrow to turn back into a Square.",
 			//4
-			"While you are a Square, press Space to jump.\nSquare can also jump off walls!",
+			"While you are a Square, press Space to jump.",
 			//5
-			"While you are in the air,\npress Right Arrow to turn into a Triangle",
+			"Square can also jump off walls!",
 			//6
-			"Triangles can glide through the air, use A and D to tilt!",
+			"While you are in the air,\npress Right Arrow to turn into a Triangle",
 			//7
-			"Throughout your quests, its important to remember these abilities.",
+			"Triangles can glide through the air, use A and D to tilt!",
 			//8
+			"Throughout your quests, its important\nto remember these abilities.",
+			//9
 			"In each new place you visit, you must try to enter the red gate.",
 			
 	};
@@ -42,11 +44,12 @@ public class TutorialLevel extends Level implements KeyListener {
 		f = new Font("", Font.PLAIN, 20);
 	}
 	int tickTilNext = 0;
-	static final int DELAY = 50;
+	static final int DELAY = 150;
 //	int timeout = 100;
 	Font f;
 	@Override
 	public void draw (Graphics2D g) {
+//		System.out.println(nextState - state);
 		super.draw(g);
 		g.setFont(f);
 		String [] lines;
@@ -56,25 +59,32 @@ public class TutorialLevel extends Level implements KeyListener {
 			lines = help[state].split("\n");
 		}
 //		timeout = Math.max(0, timeout - 1);
+		boolean greater = tickTilNext > 0;
 		tickTilNext = Math.max(0, tickTilNext - 1);
+		if (tickTilNext == 0 && greater && nextState > 0) {
+			state = nextState;
+		}
 //		System.out.println(tickTilNext);
 		
-		g.setColor(new Color(255, 0, 0, 255 * Math.abs(tickTilNext - DELAY / 2) / (DELAY / 2)));
+		g.setColor(new Color(255, 0, 0, Math.min(255, 255 * Math.abs(DELAY / 2 - tickTilNext) / (DELAY / 4))));
 		for (int i = 0; i < lines.length; i++) {
 			g.drawString(lines[i], 250, 200 + g.getFontMetrics().getHeight() * i);
 		}
 		
 	}
 	boolean noReset; // force the user to release the key before advancing twice.
+	int nextState = -1;
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if (noReset) return;
-		noReset = true;
+		System.out.println("state: " + state +" next: " + nextState);
+		if (nextState != state && nextState > 0) return;
+		nextState = -1;
+//		if (noReset) return;
+//		noReset = true;
 //		if (timeout -- > 0) return; // initial so you dont miss anything
-		if (tickTilNext-- > 0) return;
 		int prevState = state;
 		switch (state) {
-		case 7: goal = new Rectangle(600, 400, 100, 100);
+		case 8: goal = new Rectangle(600, 400, 100, 100);
 		case 0:	
 //			timeout += 50;
 		case 1: state++;
@@ -89,20 +99,27 @@ public class TutorialLevel extends Level implements KeyListener {
 			if (e.getKeyCode() == KeyEvent.VK_SPACE) state ++;
 			break;
 		case 5:
+			if (e.getKeyCode() == KeyEvent.VK_SPACE && (LD35.me.player.onLeft || LD35.me.player.onRight)) state++;
+		case 6:
 			if (!LD35.me.player.onGround && e.getKeyCode() == KeyEvent.VK_RIGHT) state++;
 			break;
-		case 6:
+		case 7:
 			if (!LD35.me.player.onGround && LD35.me.player.type == Player.TRIANGLE && (e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_D)) state++;
 			break;
 		}
 		if (prevState != state) {
+			if (tickTilNext > 0) {
+				nextState = state;
+				state = prevState;
+			} 
+			else 
 			tickTilNext = DELAY;
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		noReset = false;
+//		noReset = false;
 	}
 
 	@Override
